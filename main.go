a3760cb7197682b25ca9376c7d1a2f80b9f018a9
cmechenie <cristian.mechenie@gmail.com>
2016-12-04 13:22:24 +0200

    initial version

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"net"
 	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
 	"strings"
+	"time"
 )
 
 func exists(domain string) (bool, error) {
 	const whoisServer = "com.whois-servers.net"
-	conn, err := net.Dial("tcp", whoisServer + ":43")
+	conn, err := net.Dial("tcp", whoisServer+":43")
 	if err != nil {
 		return false, err
 	}
@@ -20,5 +24,21 @@ func exists(domain string) (bool, error) {
 			return false, nil
 		}
 	}
-	return true, nil 
+	return true, nil
+}
+
+var marks = map[bool]string{true: "Yes", false: "No"}
+
+func main() {
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		domain := s.Text()
+		fmt.Print(domain, " ")
+		exist, err := exists(domain)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(marks[!exist])
+		time.Sleep(1 * time.Second)
+	}
 }
